136449e88c4c0b1608bbd932455a63603b22f7d6
Nikushka <veronica.ryzhkova@gmail.com>
2020-07-28 23:18:23 +0700

    добавлен список задач и кнопка загрузки

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 const render = function (container, template, place) {
     container.insertAdjacentHTML(place, template);
 }
+const TASKS_COUNT = 5;
 const mainContentElement = document.querySelector('.main');
 
 const createMenuTemplate = () => {
@@ -99,5 +100,78 @@ const createFilterTemplate = () => {
         </section>`
     );
 };
-
 render(mainContentElement, createFilterTemplate(), 'beforeend');
+
+const createBoardWrapTemplate = () => {
+    return (
+        `<section class="board container"></section>`
+    );
+};
+render(mainContentElement, createBoardWrapTemplate(), 'beforeend');
+const boardWrapElement = mainContentElement.querySelector('.board');
+const createBoardFilterTemplate = () => {
+    return (
+        `<div class="board__filter-list">
+            <a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a>
+            <a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a>
+            <a href="#" class="board__filter" data-sort-type="date-down">SORT BY DATE down</a>
+        </div>`
+    );
+};
+render(boardWrapElement, createBoardFilterTemplate(), 'afterbegin');
+const createTasksWrapTemplate = () => {
+    return (
+        `<div class="board__tasks"></div>`
+    );
+};
+const createTaskTemplate = () => {
+    return (
+        `<article class="card card--yellow">
+            <div class="card__form">
+            <div class="card__inner">
+                <div class="card__control">
+                <button type="button" class="card__btn card__btn--edit">
+                    edit
+                </button>
+                <button type="button" class="card__btn card__btn--archive">
+                    archive
+                </button>
+                <button
+                    type="button"
+                    class="card__btn card__btn--favorites card__btn--disabled"
+                >
+                    favorites
+                </button>
+                </div>
+
+                <div class="card__color-bar">
+                <svg class="card__color-bar-wave" width="100%" height="10">
+                    <use xlink:href="#wave"></use>
+                </svg>
+                </div>
+
+                <div class="card__textarea-wrap">
+                <p class="card__text">Example task with custom color and without date.</p>
+                </div>
+
+                <div class="card__settings">
+                <div class="card__details">
+                </div>
+                </div>
+            </div>
+            </div>
+        </article>`
+    );
+};
+render(boardWrapElement, createTasksWrapTemplate(), 'beforeend');
+const tasksWrapElement = boardWrapElement.querySelector('.board__tasks');
+for (let i = 0; i < TASKS_COUNT; i++) {
+    render(tasksWrapElement, createTaskTemplate(), 'afterbegin');
+}
+
+const createLoadBtnTemplate = () => {
+    return (
+        `<button class="load-more" type="button">load more</button>`
+    );
+};
+render(boardWrapElement, createLoadBtnTemplate(), 'beforeend');
