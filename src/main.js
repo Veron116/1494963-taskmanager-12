d2901af75f3b794b61c07efcd1616bd8952b8b2e
Nikushka <veronica.ryzhkova@gmail.com>
2020-08-07 22:24:24 +0700

    добавлен фильтр

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {menu} from './view/menu.js';
-import {filter} from './view/filter.js';
+import {createFilterTemplate} from './view/filter.js';
 import {boardWrap} from './view/board-wrap.js';
 import {boardFilter} from './view/board-filter';
 import {tasksWrap} from './view/tasks-wrap';
@@ -7,19 +7,20 @@ import {createTaskTemplate} from './view/task';
 import {createEditTask} from './view/edit-task';
 import {loadBtn} from './view/load-btn';
 import {generateTask} from './mock/task';
+import {generateFilter} from './mock/filter';
 
 const render = function (container, template, place) {
   container.insertAdjacentHTML(place, template);
 };
 
-const TASKS_COUNT = 3;
+const TASKS_COUNT = 15;
 const tasks = new Array(TASKS_COUNT).fill().map(generateTask);
-console.log(tasks);
+const filters = generateFilter(tasks);
 
 const mainContentElement = document.querySelector(`.main`);
 const menuWrapElement = mainContentElement.querySelector(`.main__control`);
 render(menuWrapElement, menu(), `beforeend`);
-render(mainContentElement, filter(), `beforeend`);
+render(mainContentElement, createFilterTemplate(filters), `beforeend`);
 render(mainContentElement, boardWrap(), `beforeend`);
 const boardWrapElement = mainContentElement.querySelector(`.board`);
 render(boardWrapElement, boardFilter(), `afterbegin`);
---
+++ src/mock/filter.js
@@ -0,0 +1,31 @@
+import {isExpiredTask, isRepeatingTask, humanizeTaskDueDate, isTaskExpiringToday} from '../utils';
+
+const taskToFilterMap = {
+  all: tasks => {
+    return tasks.filter(task => !task.isArchive).length;
+  },
+  archive: tasks => {
+    return tasks.filter(task => task.isArchive).length;
+  },
+  favorites: tasks => {
+    return tasks.filter(task => !task.isArchive).filter(task => task.isFavorite).length;
+  },
+  repeating: tasks => {
+    return tasks.filter(task => !task.isArchive).filter(task => isRepeatingTask(task.repeating)).length;
+  },
+  today: tasks => {
+    return tasks.filter(task => !task.isArchive).filter(task => isTaskExpiringToday(task.dueDate)).length;
+  },
+  overdue: tasks => {
+    return tasks.filter(task => !task.isArchive).filter(task => isExpiredTask(task.dueDate)).length;
+  },
+};
+
+export const generateFilter = tasks => {
+  return Object.entries(taskToFilterMap).map(([filterName, countTasks]) => {
+    return {
+      name: filterName,
+      count: countTasks(tasks),
+    };
+  });
+};
