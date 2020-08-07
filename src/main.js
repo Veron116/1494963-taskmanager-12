3c06d76413705fab7ad64d2241043aab44f86c36
Nikushka <veronica.ryzhkova@gmail.com>
2020-08-07 22:47:54 +0700

    добавлена загрузка карточек

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import {boardFilter} from './view/board-filter';
 import {tasksWrap} from './view/tasks-wrap';
 import {createTaskTemplate} from './view/task';
 import {createEditTask} from './view/edit-task';
-import {loadBtn} from './view/load-btn';
+import {createLoadBtnTemplate} from './view/load-btn';
 import {generateTask} from './mock/task';
 import {generateFilter} from './mock/filter';
 
@@ -14,6 +14,8 @@ const render = function (container, template, place) {
 };
 
 const TASKS_COUNT = 15;
+const TASK_COUNT_PER_STEP = 8;
+
 const tasks = new Array(TASKS_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
 
@@ -27,7 +29,27 @@ render(boardWrapElement, boardFilter(), `afterbegin`);
 render(boardWrapElement, tasksWrap(), `beforeend`);
 const tasksWrapElement = boardWrapElement.querySelector(`.board__tasks`);
 render(tasksWrapElement, createEditTask(tasks[0]), `afterbegin`);
-for (let i = 0; i < TASKS_COUNT; i++) {
+for (let i = 0; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
   render(tasksWrapElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
-render(boardWrapElement, loadBtn(), `beforeend`);
+
+if (tasks.length > TASK_COUNT_PER_STEP) {
+  let renderedTaskCount = TASK_COUNT_PER_STEP;
+
+  render(boardWrapElement, createLoadBtnTemplate(), `beforeend`);
+
+  const loadBtn = boardWrapElement.querySelector(`.load-more`);
+
+  loadBtn.addEventListener(`click`, event => {
+    event.preventDefault();
+    tasks
+      .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+      .forEach(task => render(tasksWrapElement, createTaskTemplate(task), `beforeend`));
+
+    renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= tasks.length) {
+      loadBtn.remove();
+    }
+  });
+}
--- src/view/load-btn.js
+++ src/view/load-btn.js
@@ -1,3 +1,3 @@
-export const loadBtn = () => {
+export const createLoadBtnTemplate = () => {
   return `<button class="load-more" type="button">load more</button>`;
 };
