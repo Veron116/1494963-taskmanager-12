dbd47903a40027a8543667ca0e019e6e31f2f09b
Nikushka <veronica.ryzhkova@gmail.com>
2020-08-15 00:25:38 +0700

    добавлен рендер no-tasks и main.js сгруппирован на две основные функции рендера тикетов и остальных компонентов

--- src/main.js
+++ src/main.js
@@ -6,11 +6,12 @@ import TaskEditView from './view/edit-task';
 import LoadMoreButtonView from './view/load-btn';
 import BoardWrapView from './view/board-wrap';
 import TaskListView from './view/tasks-wrap';
+import NoTaskView from './view/no-task';
 import {generateTask} from './mock/task';
 import {generateFilter} from './mock/filter';
 import {render, renderPosition} from './utils';
 
-const TASKS_COUNT = 15;
+const TASKS_COUNT = 22;
 const TASK_COUNT_PER_STEP = 8;
 
 const tasks = new Array(TASKS_COUNT).fill().map(generateTask);
@@ -59,43 +60,47 @@ const renderTask = (taskListElement, task) => {
   render(taskListElement, taskComponent.getElement(), renderPosition.BEFOREEND);
 };
 
+const renderBoard = (boardContainer, boardTasks) => {
+  const boardComponent = new BoardWrapView();
+  const taskListComponent = new TaskListView();
+
+  render(boardContainer, boardComponent.getElement(), renderPosition.BEFOREEND);
+  render(boardComponent.getElement(), taskListComponent.getElement(), renderPosition.BEFOREEND);
+
+  if (boardTasks.every((task) => task.isArchive)) {
+    render(boardComponent.getElement(), new NoTaskView().getElement(), renderPosition.AFTERBEGIN);
+    return;
+  }
+
+  render(boardComponent.getElement(), new SortView().getElement(), renderPosition.AFTERBEGIN);
+
+  boardTasks
+    .slice(0, Math.min(tasks.length, TASK_COUNT_PER_STEP))
+    .forEach((boardTask) => renderTask(taskListComponent.getElement(), boardTask));
+
+  if (boardTasks.length > TASK_COUNT_PER_STEP) {
+    let renderedTaskCount = TASK_COUNT_PER_STEP;
+
+    const loadMoreButtonComponent = new LoadMoreButtonView();
+
+    render(boardComponent.getElement(), loadMoreButtonComponent.getElement(), renderPosition.BEFOREEND);
+
+    loadMoreButtonComponent.getElement().addEventListener(`click`, (event) => {
+      event.preventDefault();
+      boardTasks
+        .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+        .forEach((boardTask) => renderTask(taskListComponent.getElement(), boardTask));
+
+      renderedTaskCount += TASK_COUNT_PER_STEP;
+
+      if (renderedTaskCount >= tasks.length) {
+        loadMoreButtonComponent.getElement().remove();
+        loadMoreButtonComponent.removeElement();
+      }
+    });
+  }
+};
+
 render(siteHeaderElement, new SiteMenuView().getElement(), renderPosition.BEFOREEND);
 render(siteMainElement, new FilterView(filters).getElement(), renderPosition.BEFOREEND);
-
-const boradComponent = new BoardWrapView();
-render(siteMainElement, boradComponent.getElement(), renderPosition.BEFOREEND);
-render(boradComponent.getElement(), new SortView().getElement(), renderPosition.AFTERBEGIN);
-
-const taskListComponent = new TaskListView();
-render(boradComponent.getElement(), taskListComponent.getElement(), renderPosition.BEFOREEND);
-render(taskListComponent.getElement(), new TaskEditView(tasks[0]).getElement(), renderPosition.BEFOREEND);
-
-for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
-  renderTask(taskListComponent.getElement(), tasks[i]);
-}
-
-if (tasks.length > TASK_COUNT_PER_STEP) {
-  let renderedTaskCount = TASK_COUNT_PER_STEP;
-
-  const loadMoreButtonComponent = new LoadMoreButtonView();
-
-  render(boradComponent.getElement(), loadMoreButtonComponent.getElement(), renderPosition.BEFOREEND);
-
-  loadMoreButtonComponent.getElement().addEventListener(`click`, (event) => {
-    event.preventDefault();
-    tasks
-      .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-      .forEach((task) => renderTask(taskListComponent.getElement(), task));
-
-    renderedTaskCount += TASK_COUNT_PER_STEP;
-
-    if (renderedTaskCount >= tasks.length) {
-      loadMoreButtonComponent.getElement().remove();
-      loadMoreButtonComponent.removeElement();
-    }
-  });
-}
-
-// renderTemplate(mainContentElement, new FilterView(filters).getElement(), renderPosition.BEFOREEND);
-// renderTemplate(boardWrapElement, createTasksWrap(), `beforeend`);
-// const tasksWrapElement = boardWrapElement.querySelector(`.board__tasks`);
+renderBoard(siteMainElement, tasks);
